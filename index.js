dc02b7b63a6b0d1ac4b58b7a0c6113d586c360e7
ikeboy003 <102985514+ikeboy003@users.noreply.github.com>
2023-01-07 14:57:33 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-var incomeCount = 1;
 function addIncomeItem() {
   let incomeName = document.getElementById("income-name").value;
   let incomeAmount = document.getElementById("income-amount").value;
@@ -8,9 +7,21 @@ function addIncomeItem() {
   var cell1 = row.insertCell(0);
   var cell2 = row.insertCell(1);
   var cell3 = row.insertCell(2);
-  cell1.innerHTML = ++count;
+
   cell2.innerHTML = incomeName;
-  cell3.innerHTML = incomeAmount;
+  cell3.innerHTML = "$" + incomeAmount;
 }
 
-function addExpenseItem() {}
+function addExpenseItem() {
+  let expenseName = document.getElementById("expense-name").value;
+  let expenseAmount = document.getElementById("expense-amount").value;
+
+  var tableRow = document.getElementById("expense-table");
+  var row = tableRow.insertRow(-1);
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  var cell3 = row.insertCell(2);
+
+  cell2.innerHTML = expenseName;
+  cell3.innerHTML = "$" + expenseAmount;
+}
